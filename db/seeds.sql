d625a45a40d0fcb2feed6e4d990c3b93d92da73f
Naji Dia <najimdia@gmail.com>
2021-12-16 14:24:08 -0800

    project docs

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department (name)
+VALUES ("Algebra I"),
+       ("Pre-Calculus"),
+       ("Calculus I"),
+       ("Euclidean Geometry");
+       
+INSERT INTO role (title, salary, department_id)
+VALUES ("Algebra I", "Linear equations, inequalities, functions, and graphs", true),
+       ("Pre-Calculus", "Polynomials, Complex Numbers, Vectors", true),
+       ("Calculus I", "Limits, Differentiation, Derivatives", true),
+       ("Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES (Naji, Dia, 123, 145),
+       (Jihad, Moubayed, 169, 181),
+       (),
+       ("Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
+       

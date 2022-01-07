66d8a8a6ae7200340f77dad88a9459fa529f2c34
Naji Dia <najimdia@gmail.com>
2022-01-06 16:55:32 -0800

    final commits

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # HW10-Employee-Management-System
+## Description
+- The motivation of this project was to learn how to integrate a database within a command line interface.
+- Built this project to deepen knowledge on promises, mysql, and inquirer. 
+- It solves utilizing connections between databases and an application interface.
+- Learned how to utilize mysql along with a npm library to create a user contributed application.   
+
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Features](#features)
+
+## Usage
+
+![Note Taker Gif](gif1.gif)
+
+## Credits
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Features
+- Allows user to manage employee system within the command line.
+- Different features such as viewing, adding, and deleting from multiple tables within the backend database.
+- List style prompt that allows for a cleaner user interaction.
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,33 @@
-INSERT INTO department (name)
-VALUES ("Algebra I"),
-       ("Pre-Calculus"),
-       ("Calculus I"),
-       ("Euclidean Geometry");
-       
-INSERT INTO role (title, salary, department_id)
-VALUES ("Algebra I", "Linear equations, inequalities, functions, and graphs", true),
-       ("Pre-Calculus", "Polynomials, Complex Numbers, Vectors", true),
-       ("Calculus I", "Limits, Differentiation, Derivatives", true),
-       ("Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
-       
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES (Naji, Dia, 123, 145),
-       (Jihad, Moubayed, 169, 181),
-       (),
-       ("Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
-       
+use employee_db;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Naji', 'Dia', 1, NULL),
+    ('Jihad', 'Moubayed', 2, 1),
+    ('Ahmed', 'Serafi', 3, NULL),
+    ('Moe', 'Ziq', 4, 3),
+    ('Noor', 'Moubayed', 5, NULL),
+    ('Herbert', 'Pham', 6, 5),
+    ('Nidal', 'Moubayed', 7, NULL),
+    ('Muhammed', 'Hamza', 8, 7);
